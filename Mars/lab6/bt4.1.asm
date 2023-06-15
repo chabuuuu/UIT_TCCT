9df67975a50fef4ca4a0a7199a0e34e4b6646e2f
chabuuuu <87564111+chabuuuu@users.noreply.github.com>
2023-06-15 13:56:53 +0700

    lab6

--- Mars/lab6/bt4.1.asm
+++ Mars/lab6/bt4.1.asm
@@ -6,26 +6,20 @@ d: .word 0
 
 .text
 main:
-    # Khởi tạo giá trị cho a và b
-    addiu $t0, $t3, 6
-    li $t0, 6
+    addiu $t0, $zero, 6 
     sw $t0, a
     
-    li $t0, 5
+    addiu $t0, $zero, 5
     sw $t0, b
 
-    # Tính toán giá trị c = a - b
+
     lw $t0, a
     lw $t1, b
     sub $t2, $t0, $t1
     sw $t2, c
-
-    # Tính toán giá trị d = a + b
-    lw $t0, a
-    lw $t1, b
+    
     add $t2, $t0, $t1
     sw $t2, d
 
-    # Kết thúc chương trình
-    li $v0, 10
-    syscall
+
+
---
+++ Mars/lab6/bt4.2.asm
@@ -0,0 +1,7 @@
+j equal
+li $s3, 5
+equal:
+li $s2, 1
+
+
+
